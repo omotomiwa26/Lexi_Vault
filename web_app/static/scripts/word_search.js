4c4ed04fcd7d04911b7a4b3261277f7b91ed69de
omotomiwa26 <omotomiwa26@gmail.com>
2024-03-07 17:39:04 +0100

    modified word search js file

---
+++ web_app/static/scripts/word_search.js
@@ -0,0 +1,8 @@
+function searchWord() {
+    // Get the input value
+    var word = document.getElementById("wordInput").value;
+    // Simulate searching (replace with actual search functionality)
+    var result = "Definition of " + word + " goes here.";
+    // Display the result
+    document.getElementById("searchResult").innerText = result;
+}
