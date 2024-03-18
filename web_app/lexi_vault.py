b3d1c36dd1479965fc636030e9bcddfa24f566fd
omotomiwa26 <omotomiwa26@gmail.com>
2024-03-18 21:59:24 +0100

    modified flask file to add text to speech

--- web_app/lexi_vault.py
+++ web_app/lexi_vault.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy 
 from sqlalchemy import create_engine
 
@@ -41,19 +41,33 @@ def lexi_vault():
         if word:
             dictionary_data = dictionary.query.filter_by(words=word).first()
             if dictionary_data:
+                word = dictionary_data.words
                 meaning = dictionary_data.meanings
                 part_of_speech = dictionary_data.part_of_speech
             else:
-                meaning = 'Word not found'
-                part_of_speech = ''
+                meaning = 'Word Not Found'
+                part_of_speech = 'Please add to list of suggested words'
             # all_words_data = dictionary.query.with_entities(dictionary.words).all()
             # all_words = '\n'.join(word[0] for word in dictionary_data)
-            return render_template('lexi_vault.html',all_words=all_words, meaning=meaning, part_of_speech=part_of_speech)
+            return render_template('lexi_vault.html', word=word, meaning=meaning, part_of_speech=part_of_speech, all_words=all_words)
             # dictionary_data = dictionary.query.filter_by(words='word').all()
             # return render_template('lexi_vault.html', dictionary=dictionary_data)
     return render_template('lexi_vault.html', all_words=all_words)
     #     #     return render_template('lexi_vault.html', dictionary='Word not found')
     # #dictionary_data = dictionary.query.all()
+
+
+@app.route('/get_word_details', methods=['POST'])
+def get_word_details():
+    word = request.form.get('wordtext')
+    if word:
+        dictionary_data = dictionary.query.filter_by(words=word).first()
+        if dictionary_data:
+            meaning = dictionary_data.meanings
+            part_of_speech = dictionary_data.part_of_speech
+            return jsonify({'meaning': meaning, 'part_of_speech': part_of_speech})
+    return jsonify({'error': 'Word not found'})
+
             
     
 
